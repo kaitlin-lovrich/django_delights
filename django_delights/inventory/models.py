9ea5b202933014d0765e094dff31fcd39cac1564
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-02-21 18:18:07 -0800

    Create Inventory model and add to admin.py

--- django_delights/inventory/models.py
+++ django_delights/inventory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
+    unit = models.CharField(max_length=200)
+    unit_price = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
