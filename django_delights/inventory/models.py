741bedfbf46a6cd70a5044d169b10a4608c5f61f
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-02-22 14:45:10 -0800

    Create MenuItem model and add to admin.py

--- django_delights/inventory/models.py
+++ django_delights/inventory/models.py
@@ -9,3 +9,11 @@ class Ingredient(models.Model):
     
     def __str__(self):
         return self.name
+        
+class MenuItem(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
+        
