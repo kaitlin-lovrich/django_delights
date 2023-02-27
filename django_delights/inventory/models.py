d15c010955746321ff3c38d363d9e28c6b381be7
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-02-27 14:30:57 -0800

    Create Purchase model and add to admin.py

--- django_delights/inventory/models.py
+++ django_delights/inventory/models.py
@@ -17,7 +17,16 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.title
         
-class RecipeRequirements(models.Model):
+class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.menu_item + ': ' + self.ingredient
+        # try return self.menu_item + ': ' self.quantity + ' ' self.ingredient.unit + ' of ' + self.ingredient after we know the code works to make RecipeRequirement more readable
+        
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    customer_name = models.CharField(max_length=200, blank=True)
